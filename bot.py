62d4f5ee30d3708b30eb1ce7ef7a347c1a9e97a4
Diogo Costa <diogocosta876@hotmail.com>
2022-02-24 02:33:55 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,18 +16,20 @@ async def on_message(message):
         return
 
     if random.randint(1, 20) == 1:
-        await message.channel.send('dizes', tts=False)
+        await message.channel.send('hey! this is a random response to a message', tts=False)
 
     if message.content.startswith("!megadizes"):
         await message.channel.send('''\
     ```
 
-    .___.__                      
-  __| _/|__|_______ ____   ______
- / __ | |  \___   // __ \ /  ___/
-/ /_/ | |  |/    /\  ___/ \___ \ 
-\____ | |__/_____ \\___  >____  >
-     \/          \/    \/     \/ 
+888    888 8888888888 Y88b   d88P 
+888    888 888         Y88b d88P  
+888    888 888          Y88o88P   
+8888888888 8888888       Y888P    
+888    888 888            888     
+888    888 888            888     
+888    888 888            888     
+888    888 8888888888     888
 
 ```''')
 
